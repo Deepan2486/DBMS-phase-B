cc15bf11c2b246225b6411bc701da4bfc1eaf12f
Deepan Maitra <2019csb1044@iitrpr.ac.in>
2021-11-16 01:57:27 +0530

    Update draftB.sql

--- draftB.sql
+++ draftB.sql
@@ -10,3 +10,27 @@ CREATE TABLE production_company(
 	address varchar(30)
 
 );
+
+
+CREATE TABLE movie(
+
+	m_id INT UNIQUE PRIMARY KEY,
+	name varchar(10),
+	year INT,
+	imbd_score numeric(3,2),
+	prod_company INT,
+	
+	CONSTRAINT fk_pid FOREIGN KEY(prod_company) REFERENCES production_company(pc_id)
+);
+
+
+CREATE TABLE casting(
+	
+	m_id INT,
+	a_id INT,
+	
+	CONSTRAINT fk_mid FOREIGN KEY(m_id) REFERENCES movie(m_id),
+	CONSTRAINT fk_aid FOREIGN KEY(a_id) REFERENCES actor(a_id),
+	
+	PRIMARY KEY (m_id, a_id)
+);
